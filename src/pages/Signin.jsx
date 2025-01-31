9c18d7f2d1287c745fdeff7c7142fc3ecc9648ed
amit04-07-1999 <ameykumar76@gmail.com>
2025-01-31 17:47:18 +0530

    tets

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -166,8 +166,8 @@ const Signin = () => {
                           onChange={handleChange}
                         >
                           <option value="">Select Role</option>
-                          <option value="superadmin">Super Admin</option>
-                          <option value="admin">Admin</option>
+                          <option value="superadmin"> Admin</option>
+                          {/* <option value="admin"></option> */}
                         </select>
                       </div>
                     </div>
