baba43d745ddedb2c817cb39a6fa98937ed4d9fa
MD Sharik <md.sharik5012@gmail.com>
2025-02-10 16:32:58 +0530

    4:32 pm

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [react()],
   optimizeDeps: {
-    include: ['fabric'],
+    include: ["fabric"],
+  },
+  server: {
+    host: "192.168.1.24",
+    port: 3000,
+    https: {
+      key: fs.readFileSync("../BACKEND/certificates/key.pem"),
+      cert: fs.readFileSync("../BACKEND/certificates/cert.pem"),
+    },
   },
 });
